3eb2909269833f62f3cd5bee682c6f940e9ecf99
eduardfores <eduardfores@gmail.com>
2025-05-16 22:45:50 +0200

    add dpendencies

--- src/main/java/org/zeusagents/Main.java
+++ src/main/java/org/zeusagents/Main.java
@@ -1,4 +1,4 @@
-package org.zeusagents.agents;
+package org.zeusagents;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
